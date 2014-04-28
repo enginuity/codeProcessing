8d18ed7b2fb412455f56be2efd5c679150ac5f60
Lawrence Wang <625lawrence@gmail.com>
2014-04-27 22:56:27 -0400

    update netcomp project example.

---
+++ netcomp_setup.R
@@ -0,0 +1,8 @@
+##@S This is examples of how to run this for the netcomp project.
+
+## Load all available programs
+source("load_all.R")
+
+
+## Search code (and add comments)
+search_code(dir = "../network-comparison/netcomp-project/", mode = "R", regexp = "sim_one", add_comment = "Test this..")
