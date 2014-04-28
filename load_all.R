e92bf77337dafc7b06713148c053feae02164468
Lawrence Wang <625lawrence@gmail.com>
2014-04-27 22:42:22 -0400

    reorganize directory

---
+++ load_all.R
@@ -0,0 +1,7 @@
+##@S This loads all the functions (so running this script would load all the written functions...
+
+## Load helper functions
+source("sub_programs/text_proc_fxs.R")
+
+## Load code-searching tools
+source("sub_programs/search_code.R")
