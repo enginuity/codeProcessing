3b7fa8735b046f28987b3fe18d6809fcad72c566
Lawrence Wang <625lawrence@gmail.com>
2014-07-11 00:09:43 -0400

    create package skeleton

---
+++ metacode/R/file_proc_fxs.R
@@ -0,0 +1,27 @@
+##@S This file contains function to aid with repeating file processing tasks (file-name processing, etc.)
+
+find_files = function(dir = ".", mode = c("R","C"), file_regex = NULL) {
+  ## TODO: [Document] this function
+  res = list.files(path = dir, recursive = TRUE, full.names = TRUE)
+
+  ## Find files with correct filename extension
+  if (mode == "R") {
+    matches = grep("[.]R$", res)
+    comment_head = "\n  ##**##----- "
+  } else if (mode == "C") {
+    matches = grep("[.](c|cc|cpp|h|hh)$", res)
+    comment_head = "\n  //**##----- "
+  } else {
+    stop("Invalid mode (Not \"R\" or \"C\"")
+  }
+  res = res[matches]
+
+  ## Apply regex of file_regex when appropriate
+  ## TODO: This only has the functionality ot look at the entire filename. 
+  if (!is.null(file_regex)) {
+    matches = grep(file_regex, res)
+    res = res[matches]
+  }
+
+  return(res)
+}
---
+++ metacode_setup.R
@@ -0,0 +1,12 @@
+##@S This is examples of how to run this for the netcomp project.
+
+DIR = "metacode/"
+
+## Load all available programs
+source("load_all.R")
+
+
+create_roxy_templates(dir = DIR)
+
+
+
