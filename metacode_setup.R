faf788d144f2b292f7325273c296b6aa3791a88f
Lawrence Wang <625lawrence@gmail.com>
2014-07-11 03:38:58 -0400

    update metacode

--- metacode_setup.R
+++ metacode_setup.R
@@ -5,8 +5,8 @@ library(metacode)
 library(stringr)
 
 # For fixing templates
-DIR = "metacode/"
-create_roxy_templates(dir = DIR)
+# DIR = "metacode/"
+# create_roxy_templates(dir = DIR)
 
 search_code(mode = "R", regexp = "find_files", add_comment = "*** This function is modified to return what used to be 'allcode', so all functions need to be edited. ")
 #|----##*** This function is modified to return what used to be 'allcode', so all functions need to be edited.  --Fri Jul 11 02:42:44 2014--
