1ebb1833b3770b22b64c839ad38ff5e1a827f47e
Lawrence Wang <625lawrence@gmail.com>
2014-07-11 00:46:18 -0400

    build package

---
+++ compile_pkg_metacode.R
@@ -0,0 +1,15 @@
+
+detach(package:metacode)
+library(roxygen2)
+roxygenise("metacode/", clean = TRUE)
+
+system("R CMD build metacode")
+
+## For windows 
+system("R CMD check metacode_1.0.tar.gz")
+system("R CMD INSTALL metacode")
+
+## For unix
+#  install.packages(pkgs="metacode_1.0.tar.gz")
+
+library(metacode)
