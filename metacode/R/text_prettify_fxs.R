40f491ac5a43ed6dc415ac08e7a301ebe4f9410b
Lawrence Wang <625lawrence@gmail.com>
2014-07-11 01:37:36 -0400

    Fix documentation for text_proc, and rename it to text_prettify

---
+++ metacode/R/text_prettify_fxs.R
@@ -0,0 +1,56 @@
+##@S This file contains generic text-prettifying functions
+
+#' Creates a character vector of a varying amount of 'ch' characters
+#' This is mainly used to space out text for pretty displaying (in printing dataframes)
+#' 
+#' Example: If v = c(5,1), returns c("     ", " ")
+#' 
+#' @param v numeric vector
+#' @param ch character to repeat
+#' 
+#' @return character vector of length(v)
+#' 
+#' @export
+create_space_chars = function(v, ch = " ") {
+  toreturn = as.character(v)
+  for (j in 1:length(v)) {
+    toreturn[j] = paste(rep(ch, times = v[j]), collapse = "")
+  }
+  return(toreturn)
+}
+
+#' For elements of \code{t}, adjust its number of characters by prepending/appending/removing
+#' 
+#' @param t character vector to extend or shorten
+#' @param len target length
+#' @param at_end TRUE/FALSE: modify entries at beginning or at the end
+#' 
+#' @return character vector, each with nchar equal to \code{len}
+#' 
+#' @export
+fix_length = function(t, len, at_end = TRUE) {  
+  fix_length_single = function(t_single, len, at_end = TRUE) {
+    # Does fix_length for a singleton
+    t_len = nchar(t_single)
+    if (t_len < len) {
+      if (at_end) {
+        return(paste(c(t_single, 
+                       rep(" ", times = len - t_len)), 
+                     collapse = "") )
+      } else {
+        return(paste(c(rep(" ", times = len - t_len), 
+                       t_single), 
+                     collapse = "") )
+      }
+    } else if (t_len > len) {
+      if (at_end) {
+        return(substr(t_single, start = 1, stop = len))
+      } else {
+        return(substr(t_single, start = t_len - len + 1, stop = t_len))
+      }
+    } else {
+      return(as.character(t_single))
+    }
+  }
+  return(vapply(t, FUN = fix_length_single, FUN.VALUE = 'character', len = len, at_end = at_end))
+}
