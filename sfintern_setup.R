35102d9b4aad448e1bb79ab7a87951a50bd5a4ed
Lawrence Wang <625lawrence@gmail.com>
2014-07-13 00:46:40 -0400

    edit comments so that they wouldn't be removed on a clear_comments scan. removed load_all.R since it's essentially obselete.

--- sfintern_setup.R
+++ sfintern_setup.R
@@ -13,45 +13,45 @@ source("load_all.R")
 create_roxy_templates(dir="../27may_user_engagement/source/fxForUserchurn/R/")
 
 
-search_code(dir = DIR, mode = "R", regexp = "Create_features_v2", fixed = TRUE)
-#|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
-
-search_code(dir = DIR, mode = "R", regexp = "Create_features_v2", replace = "Create_features", add_comment = "Rename v2 -> raw", fixed = TRUE)
-#|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
+# search_code(dir = DIR, mode = "R", regexp = "Create_features_v2", fixed = TRUE)
+# #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
+# 
+# search_code(dir = DIR, mode = "R", regexp = "Create_features_v2", replace = "Create_features", add_comment = "Rename v2 -> raw", fixed = TRUE)
+# #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
 
 
 clear_comments(dir = DIR, mode = "R")
 
-search_code(dir=DIR, mode="R", add_comment="relabeled", file_regex="script_agg", regexp="dds", replace="tenure", fixed = TRUE)
-#|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
+# search_code(dir=DIR, mode="R", add_comment="relabeled", file_regex="script_agg", regexp="dds", replace="tenure", fixed = TRUE)
+# #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
 
 
 
 
 
-search_code(dir = DIR, mode = "R", regexp = "Create[_]features", fixed = FALSE, add_comment = "TODO: Create_features -- change usage of 'change' parameter")
-#|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
-
-
-
-search_code(dir = DIR, mode = "R", regexp = "square")
-#|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
-search_code(dir = DIR, mode = "R", regexp = "agg_fx")
-#|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
-
-search_code(dir = DIR, mode = "R", regexp = "sim_one")
-#|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
-search_code(dir = DIR, mode = "R", regexp = "HP_user")
-#|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
-
-search_code(dir = DIR, mode = "R", regexp="agg_fx", add_comment="need-rename")
-#|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
-
-
-
-
-search_code(dir=DIR, mode="R", regexp="file *= *f, *", add_comment="remove this", file_regex="script", replace="")
-#|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
+# search_code(dir = DIR, mode = "R", regexp = "Create[_]features", fixed = FALSE, add_comment = "TODO: Create_features -- change usage of 'change' parameter")
+# #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
+# 
+# 
+# 
+# search_code(dir = DIR, mode = "R", regexp = "square")
+# #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
+# search_code(dir = DIR, mode = "R", regexp = "agg_fx")
+# #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
+# 
+# search_code(dir = DIR, mode = "R", regexp = "sim_one")
+# #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
+# search_code(dir = DIR, mode = "R", regexp = "HP_user")
+# #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
+# 
+# search_code(dir = DIR, mode = "R", regexp="agg_fx", add_comment="need-rename")
+# #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
+# 
+# 
+# 
+# 
+# search_code(dir=DIR, mode="R", regexp="file *= *f, *", add_comment="remove this", file_regex="script", replace="")
+# #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
 
 
 
