6e051d5050f833c89b6b7f7b51fe452fbf04484b
Lawrence Wang <625lawrence@gmail.com>
2014-09-16 18:53:02 -0400

    update with new experiments

--- examples/netcomp_setup.R
+++ examples/netcomp_setup.R
@@ -9,6 +9,9 @@ DIR = "../network-comparison/netcomp-project/"
 ## Desktop
 DIR = "../netcomp-project/"
 
+plot_dependency(dir = "../network-comparison/netcomp-project/netcomplib/", mode = "R", out_file = "results/test.pdf")
+
+
 # ## Search code (and add comments)
 # #search_code(dir = DIR, mode = "R", regexp = "sim_one", add_comment = "Test this..")
 # #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
@@ -16,12 +19,16 @@ DIR = "../netcomp-project/"
 # #|----##this function has been split (order of args changed) --Sun Jul 13 00:43:19 2014--
 
 ## Generate todo list
+
+search_code("update_probabilities")
+
+
 # generate_todolist(dir = DIR)
 search_code("sim_results")
 search_code("single.procedure.test")
 #replace_code("depth[.]from[.]parents", "depth_from_parents", "replace . with _")
 #replace_code("display[.]tree", "display_tree", "replace . with _")
-
+search_code("cmn_mcmc", add_comment = "Updated this function (see model_mcmc.R), so need to update usages as necessary")
 
 search_code("mle_loglik", add_comment = "Need to Use updated version of mle_loglik in lrt_functions.R; more parameters to pass in")
 
