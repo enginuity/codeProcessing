e9a719efba08d32df9815e57091b4b389412a50c
Lawrence Wang <625lawrence@gmail.com>
2014-09-30 14:14:58 -0400

    Fix Issue 21: Now search_code can directly be inputted changes to comment heading.

--- metacode/R/search_code.R
+++ metacode/R/search_code.R
@@ -2,17 +2,19 @@
 ##@S   Search results are output inside this directory (metadata)
 
 
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (search_code)
 #' Search code and potentially add comments
 #' 
 #' @param RE Object of class Regex, OR a simple regular expression. What to search for?
 #' @param add_comment If non-NULL, this is added to the source code as a next-line comment
+#' @param comment_heads Length 2 vector: A short and long comment header to add to comments
 #' @param FD Object of class FilesDescription; See documentation to see how to describe a collection of files  
 #' 
 #' @return none
 #' 
 #' @export
 #' 
-search_code = function(RE, add_comment = NULL, FD = DEFAULT_FD) {
+search_code = function(RE, add_comment = NULL, comment_heads = c("#|", "#|----##"), FD = DEFAULT_FD) {
   
   ## If input is a regular expression instead of Regex object, use default settings. 
   if (class(RE) != "Regex") { RE = Regex(base = RE, isword = TRUE) }
@@ -20,7 +22,7 @@ search_code = function(RE, add_comment = NULL, FD = DEFAULT_FD) {
   ## Find matches in MatchedCodebase format
   matchesL = search_code_matches(RE = RE, FD = FD, logged = "SEARCH")
   
-  if (!is.null(add_comment)) { add_comment_matches(matchesL, add_comment, write = TRUE) }
+  if (!is.null(add_comment)) { add_comment_matches(matchesL, add_comment, comment_heads = comment_heads, write = TRUE) }
   return("Search is done!")
 }
 
