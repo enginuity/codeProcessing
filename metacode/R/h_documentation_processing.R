369746b51a1e5a1cc156a2d77661b016ad730e98
Lawrence Wang <625lawrence@gmail.com>
2014-10-01 21:02:32 -0400

    Completely redo write roxygen2 documentation

--- metacode/R/h_documentation_processing.R
+++ metacode/R/h_documentation_processing.R
@@ -3,11 +3,13 @@
 
 
 
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (Mode_nontemp)
 #' Compute Mode, ignoring certain values
 #'     
 #' Ignores all values that are equal to 'temp', and computes the mode of the remaining text values
 #' 
 #' @param text Character vector
+#' @param ignore_type temp
 #' 
 #' @return Mode of 'text'
 #' 
@@ -183,7 +185,7 @@ find_all_prev_documentation_v2 = function(text, lineno, header = "^#'") {
   if (is.na(prev_lines[1])) {
     return(NA)
   }
-  prev_docu = data.frame(LineNo=prev_lines, Value = "", Type ="", ParamName = "", TypeOrder = 0, ParamOrder = 0, SubOrder = 0, stringsAsFactors=FALSE)
+  prev_docu = data.frame(LineNo = prev_lines, Value = "", Type = "", ParamName = "", TypeOrder = 0, ParamOrder = 0, SubOrder = 0, stringsAsFactors=FALSE)
   
   text_clean = gsub(str_c(header, " +"), "", text[prev_lines])
   ## Mode is either 'Empty', 'Text', or "@something"
@@ -192,6 +194,7 @@ find_all_prev_documentation_v2 = function(text, lineno, header = "^#'") {
   ## Figure out what type each line is
   for(j in seq_along(prev_lines)) {
     t = tsplit[[j]]
+    ## TODO: [Generalize] This imposes a very rigid instruction on the format of documentation. Could be relaxed. 
     t = t[t != ""]
     if (length(t) < 1 | is.na(t[1])) {
       prev_docu$Type[j] = 'Empty'
@@ -210,15 +213,121 @@ find_all_prev_documentation_v2 = function(text, lineno, header = "^#'") {
     prev_docu$Value[j] = text[prev_lines[j]]
   }
   
-  ## Process the types... 
-  raw_docu_df = prev_docu
-  prev_docu = process_documentation(prev_docu)
   return(prev_docu)
 }
 
 
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (reformat_documentation_v2)
+#' Takes old (or non-existent) roxygen documentation, and reformats it
+#' Then outputs what it thinks the new format should be. Then, need to separately check whether it matches what it thinks it should be. 
+#' 
+#' @param cur_doc old documentation (as data frame output)
+#' @param params function parameters
+#' @param default_param_doc temp
+#' 
+#' @return character vector: correct roxygen documentation
+#' 
+#' @export
+#' 
+reformat_documentation_v2 = function(cur_doc, params, default_param_doc = NULL) {
+  
+  if (FALSE) {
+    cur_doc = NA
+  }
+  
+  reorder_rows = function(df) {
+    return(df[order(df$TypeOrder, df$ParamOrder, df$SubOrder),])
+  }
+  
+  ## General format of documentation: 
+  ## 1. Title, Description, Details. These are separated by single empty lines. 
+  ## 2. Param documentation. These lead by @param <paramname> <documentation> and can span multiple lines. 
+  ##    NO empty lines, so these will be removed. 
+  ## 3. Return documentation. This can span multiple lines. 
+  ## 10. @export. This is followed by a single line, and then the function name. This does NOT have to exist, though. 
+  ## The Numbering here is type numbering to be placed in TypeOrder. 
+  ##
+  ## General rules: 
+  ## Empty lines have a single space after #'. (This won't be done in THIS function...)
+  ## There should never be multiple empty lines in a row.   
+  
+  ## NEED TO CHECK FOR MISSING ITEMS. 
+  ## IF cur_doc is NA -> need to fill out
+  ## else if cur_doc is missing parts, need to fill out. 
+  base_doc = data.frame(LineNo = -1, Value = c("#' <What does this function do>", "#' @return temp", "#' @export"), Type = c("Text", "@return", "@export"), ParamName = "", TypeOrder = 0, ParamOrder = 0, SubOrder = 0, stringsAsFactors=FALSE)
+  
+  if (!(length(cur_doc) > 1) && is.na(cur_doc)) {
+    new_doc = base_doc
+  } else {
+    new_doc = cur_doc
+    rowstoadd = NULL
+    if (!any(new_doc$Type == "Text")) { rowstoadd = c(rowstoadd, 1) }
+    if (!any(new_doc$Type == "@return")) { rowstoadd = c(rowstoadd, 2) }
+    if (!any(new_doc$Type == "@export")) { rowstoadd = c(rowstoadd, 3) }
+    if (!is.null(rowstoadd)) { new_doc = rbind(new_doc, base_doc[rowstoadd,]) }
+  }
+  
+  n = nrow(new_doc)  
+  types = new_doc$Type
+  ## Assign arrangement of regular sections
+  ## TODO: [Refactor] This code is VERY ugly... Logic needs to be rethought... 
+  matches = find_type_section(types, start_line = 1, end_marker = c("@param", "@return"), keep_empty = TRUE)
+  new_doc$TypeOrder[matches] = 1
+  new_doc$SubOrder[matches] = seq_along(matches)
+  
+  ## IF parameters exist: 
+  if (any(types == "@param")) {
+    matches = find_type_section(types, start_line = 1 + max(which(new_doc$TypeOrder > 0)), end_marker = c("@return"))
+    new_doc$TypeOrder[matches] = 2
+    new_doc$SubOrder[matches] = seq_along(matches)
+  }
+  
+  matches = find_type_section(types, start_line = 1 + max(which(new_doc$TypeOrder > 0)), end_marker = c("@export"))
+  new_doc$TypeOrder[matches] = 3
+  new_doc$SubOrder[matches] = seq_along(matches)
+  
+  matches = find_type_section(types, start_line = 1 + max(which(new_doc$TypeOrder > 0 )), end_marker = c("endofdocu"))
+  new_doc$TypeOrder[matches] = 10
+  new_doc$SubOrder[matches] = seq_along(matches)
+  
+  ## Assign arrangement for parameters
+  if (any(new_doc$TypeOrder == 2)) {
+    for(j in seq_along(unique(new_doc$ParamName[new_doc$ParamName != ""]))) {
+      matches = find_type_section(types, start_line = min(which((new_doc$TypeOrder == 2) & (new_doc$ParamOrder == 0))), end_marker = c("@param", "@return"), keep_one = TRUE)
+      new_doc$ParamOrder[matches] = j
+      new_doc$SubOrder[matches] = seq_along(matches)
+    }
+  }
+  
+  ## Add #'s for spaces: at positions 1.5, 2.5, 3.5, 10.5
+  new_doc = rbind(new_doc, data.frame(LineNo=-1, Value = "#' ", Type ="Empty", ParamName = "", TypeOrder = c(1.5, 2.5, 3.5, 10.5), ParamOrder = 0, SubOrder = 1, stringsAsFactors=FALSE))
+  
+  ## Find correct parameter assignment
+  new_doc = reform_params(raw_docu_df = new_doc, correct_param_order = params, default_param_doc = default_param_doc)
+  
+  ## Reorder rows using ordering
+  new_doc = reorder_rows(new_doc)
+  
+  ## Check for multiple empty lines in a row. Set TypeOrder to 0
+  double_empty_rows = which((new_doc$Type[-1] == "Empty") & (new_doc$Type[-1] == new_doc$Type[-length(new_doc$Type)]))
+  if (length(double_empty_rows) > 0) { new_doc = new_doc[-double_empty_rows,] }
+  
+  return(new_doc)
+}
 
+
+
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (process_documentation)
+#' <<BasicInfo>> 
+#' 
+#' @param raw_docu_df temp
+#' 
+#' @return temp
+#' 
+#' @export
+#' 
 process_documentation = function(raw_docu_df) {
+  ## TODO: [Clear this]
   ## General format of documentation: 
   ## 1. Title, Description, Details. These are separated by single empty lines. 
   ## 2. Param documentation. These lead by @param <paramname> <documentation> and can span multiple lines. 
@@ -280,6 +389,17 @@ process_documentation = function(raw_docu_df) {
 
 
 
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (reform_params)
+#' <<BasicInfo>> 
+#' 
+#' @param raw_docu_df temp
+#' @param correct_param_order temp
+#' @param default_param_doc temp
+#' 
+#' @return temp
+#' 
+#' @export
+#' 
 reform_params = function(raw_docu_df, correct_param_order, default_param_doc = NULL) {
   ## Given a vector of the correct parameter order, reorder raw_docu_df as necessary. Add missing parameters with 'temp' documentation as necessary. 
   ## default_param_doc should be a data.frame: $ParamName & $Doc
@@ -309,7 +429,6 @@ reform_params = function(raw_docu_df, correct_param_order, default_param_doc = N
   if (length(params_toadd) > 0) {
     param_doc = rep("temp", times = length(params_toadd))
     param_doc[which(!is.na(match(params_toadd, default_param_doc$ParamName)))] = default_param_doc$Doc[match(params_toadd, default_param_doc$ParamName, nomatch = 0)]
-    print(param_doc)
     
     raw_docu_df = rbind(raw_docu_df, data.frame(LineNo=-1, Value = paste( "#' @param", param_doc), Type ="@param", ParamName = params_toadd, TypeOrder = 2, ParamOrder = param_nums, SubOrder = 1, stringsAsFactors=FALSE))
   }
@@ -323,6 +442,19 @@ reform_params = function(raw_docu_df, correct_param_order, default_param_doc = N
 
 
 
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (find_type_section)
+#' <<BasicInfo>> 
+#' 
+#' @param x temp
+#' @param start_line temp
+#' @param end_marker temp
+#' @param keep_empty temp
+#' @param keep_one temp
+#' 
+#' @return temp
+#' 
+#' @export
+#' 
 find_type_section = function(x, start_line, end_marker, keep_empty = FALSE, keep_one = FALSE) {
   if (length(which(x %in% end_marker)) == 0) {
     end_line = length(x) 
