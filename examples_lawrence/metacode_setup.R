2c9e2296f8a98041a76bc65cd8a3f23e2b18f47b
Lawrence Wang <625lawrence@gmail.com>
2015-04-03 03:56:40 -0400

    update example code

--- examples_lawrence/metacode_setup.R
+++ examples_lawrence/metacode_setup.R
@@ -4,7 +4,12 @@
 library(codeProcessing)
 package_FD = FilesDescription(mode = "R", dirlist = "codeProcessing/R/")
 
+plot_dependency(FD = package_FD, out_file = "test.pdf")
 generate_todolist(FD = package_FD)
+
+
+
+## Examples
 search_code(RE = Regex(base = "write", isexact = FALSE, isword = FALSE), FD = package_FD)
 
 
@@ -16,8 +21,6 @@ search_code("extract_param_docu", FD = package_FD)
 # examples_FD = FilesDescription(mode = "R", dirlist = c("examples/", "old_programs/"))
 # find_files(examples_FD)
 # 
-# plot_dependency(FD = package_FD, out_file = "test.pdf")
-# generate_todolist(FD = package_FD)
 # 
 # update_param_docu(test, "MCB", "Regex match list : this is output of search-code-matches", replace_all = TRUE)
 
