e415e4fd406ece029e172d27d5118a18aa6f0dbe
Lawrence Wang <625lawrence@gmail.com>
2015-04-13 21:46:06 -0400

    write new version of reformat_documentation

--- new_documentation_helpers.R
+++ new_documentation_helpers.R
@@ -44,3 +44,104 @@ process_cur_docu = function(code, lines, header = "^#'") {
   return(prev_docu)
 }
 
+#' Takes old (or non-existent) roxygen documentation, and reformats it
+#' Then outputs what it thinks the new format should be. Then, need to separately check whether it matches what it thinks it should be. 
+#' 
+#' @param cur_doc old documentation (as data frame output)
+#' @param params function parameters
+#' @param default_param_doc temp
+#' 
+#' @return character vector: correct roxygen documentation
+#' 
+#' @export
+#' 
+reformat_documentation_v2 = function(cur_doc, params, default_param_doc = NULL, to_export = TRUE) {
+  
+  reorder_rows = function(df) {
+    return(df[order(df$TypeOrder, df$ParamOrder, df$SubOrder),])
+  }
+  
+  ## General format of documentation: 
+  ## 1. Title, Description, Details. These are separated by single empty lines. 
+  ## 2. Param documentation. These lead by @param <paramname> <documentation> and can span multiple lines. 
+  ##    NO empty lines, so these will be removed. 
+  ## 3. Return documentation. This can span multiple lines. 
+  ## 10. @export. This is followed by a single line, and then the function name. This does NOT have to exist, though. 
+  ## The Numbering here is type numbering to be placed in TypeOrder. 
+  ##
+  ## General rules: 
+  ## Empty lines have a single space after #'. (This won't be done in THIS function...)
+  ## There should never be multiple empty lines in a row.   
+  
+  ## NEED TO CHECK FOR MISSING ITEMS. 
+  ## IF cur_doc is NA -> need to fill out
+  ## else if cur_doc is missing parts, need to fill out. 
+  base_doc = data.frame(LineNo = -1, Value = c("#' <What does this function do>", "#' @return temp", "#' @export"), Type = c("Text", "@return", "@export"), ParamName = "", TypeOrder = 0, ParamOrder = 0, SubOrder = 0, stringsAsFactors=FALSE)
+  
+  if (is.null(cur_doc)) {
+    new_doc = base_doc
+  } else {
+    new_doc = cur_doc
+    rowstoadd = NULL
+    if (!any(new_doc$Type == "Text")) { rowstoadd = c(rowstoadd, 1) }
+    if (!any(new_doc$Type == "@return")) { rowstoadd = c(rowstoadd, 2) }
+    if (!any(new_doc$Type == "@export")) { rowstoadd = c(rowstoadd, 3) }
+    if (!is.null(rowstoadd)) { new_doc = rbind(new_doc, base_doc[rowstoadd,]) }
+  }
+  
+  n = nrow(new_doc)  
+  types = new_doc$Type
+  
+  ## Assign arrangement of regular sections
+  ## TODO: [Refactor] This code is VERY ugly... Logic needs to be rethought... 
+  matches = find_type_section(types, start_line = 1, end_marker = c("@param", "@return"), keep_empty = TRUE)
+  new_doc$TypeOrder[matches] = 1
+  new_doc$SubOrder[matches] = seq_along(matches)
+  
+  ## IF parameters exist: 
+  if (any(types == "@param")) {
+    matches = find_type_section(types, start_line = 1 + max(which(new_doc$TypeOrder > 0)), end_marker = c("@return"))
+    new_doc$TypeOrder[matches] = 2
+    new_doc$SubOrder[matches] = seq_along(matches)
+    new_doc$TypeOrder[intersect(matches, which(types == "Empty"))] = 1000
+  }
+  
+  matches = find_type_section(types, start_line = 1 + max(which(new_doc$TypeOrder > 0)), end_marker = c("@export"))
+  new_doc$TypeOrder[matches] = 3
+  new_doc$SubOrder[matches] = seq_along(matches)
+  
+  matches = find_type_section(types, start_line = 1 + max(which(new_doc$TypeOrder > 0 )), end_marker = c("endofdocu"))
+  new_doc$TypeOrder[matches] = 10
+  new_doc$SubOrder[matches] = seq_along(matches)
+  
+  ## Assign arrangement for parameters
+  if (any(new_doc$TypeOrder == 2)) {
+    for(j in seq_along(unique(new_doc$ParamName[new_doc$ParamName != ""]))) {
+      matches = find_type_section(types, start_line = min(which((new_doc$TypeOrder == 2) & (new_doc$ParamOrder == 0))), end_marker = c("@param", "@return"), keep_one = TRUE)
+      new_doc$ParamOrder[matches] = j
+      new_doc$SubOrder[matches] = seq_along(matches)
+    }
+  }
+  
+  
+  ## Add #'s for spaces: at positions 1.5, 2.5, 3.5, 10.5
+  new_doc = rbind(new_doc, data.frame(LineNo=-1, Value = "#' ", Type ="Empty", ParamName = "", TypeOrder = c(1.5, 2.5, 3.5, 10.5), ParamOrder = 0, SubOrder = 1, stringsAsFactors=FALSE))
+  
+  ## Find correct parameter assignment
+  new_doc = reform_params(raw_docu_df = new_doc, correct_param_order = params, default_param_doc = default_param_doc)
+  
+  ## Reorder rows using ordering
+  new_doc = reorder_rows(new_doc)
+  
+  ## Check for multiple empty lines in a row. Set TypeOrder to 0
+  double_empty_rows = which((new_doc$Type[-1] == "Empty") & (new_doc$Type[-1] == new_doc$Type[-length(new_doc$Type)]))
+  if (length(double_empty_rows) > 0) { new_doc = new_doc[-double_empty_rows,] }
+  
+  
+  ## Remove export code if appropriate
+  if (!to_export) {
+    new_doc = new_doc[new_doc$TypeOrder != 10,]
+  }
+  return(new_doc)
+}
+
