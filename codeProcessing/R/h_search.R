09a2479c7903bdfd1d6d8f89b72f588b3b6c7dd7
Lawrence Wang <625lawrence@gmail.com>
2015-07-07 23:22:10 -0400

    start improving documentation

--- codeProcessing/R/h_search.R
+++ codeProcessing/R/h_search.R
@@ -1,35 +1,35 @@
-## This file contains helper files for the search code. 
+## This file contains helper files for the searching code for regular expressions. 
 
+## Documentation updated 7/7/2015 -- still needs some work!
+## Code updated ././2015
 
 ## TODO: [WriteHTMLDocument] This info should be added to documentation somehow?
 
-
 # Helper Functions --------------------------------------------------------
 
-
-
 #' Locates regex matches in codebase
-#'    
-#' This function searches the code in the 'dir'ectory (with 'file_regex' as needed, and of 'mode'). 
-#' This should be a helper function called by any function that uses searching... 
 #' 
-#' @param REGEX Object of class Regex. What to search for? 
-#' @param FD Object of class FilesDescription; See documentation to see how to describe a collection of files -- if null -> uses current directory (getwd())
-#' @param logged If non-NULL, then this is the logtype (to write in filename)
+#' All the regular expressions stored in REGEX are searched for inside the set of files described by FD. This is a helper function that should be called by any function that utilizes regular expression searching.
 #' 
-#' @return A MatchedCodebase object
+#' @param REGEX [Regex] :: Set of regular expressions and options to search for in the text/code. 
+#' @param FD [FilesDescription] :: Description of a collection of files to search the full text/code of. If this is NULL, uses current directory (by calling getwd())
+#' @param logged [char] :: If non-NULL, then this is the logtype (to write in file name of the log file)
+#' 
+#' @return [MatchedCodebase] :: Object containing information describing all REGEX matches inside all files in FD. 
 #' 
 #' @export
 #' 
 search_code_matches = function(REGEX, FD, logged = NULL) {
+  ## Ensure directory is correct: IF FD is null, default to current directory, and assume R code.  
+  if (is.null(FD)) FD = FilesDescription(mode = "R", dirlist = ".") 
   
-  ## Look for all files, that match the current mode and file_regex setting, and extract code. 
-  if (is.null(FD)) FD = FilesDescription(mode = "R", dirlist = ".") ## IF null, default to current directory. 
-  
+  ## Extract all the specified files (stored in Codebase object)
   all_code = extract_Codebase(FD = FD)
   
-  ## Matching texts:
+  ## Search all the texts for all the regular expressions given. 
   files_with_matches = which(sapply(all_code$code, function(code) {any(stringr::str_detect(code, REGEX$regex))}))
+  
+  ## Aggregate the search results
   matchline_list = list()
   matchloc_list = list()
   for(j in seq_along(files_with_matches)) {
@@ -38,27 +38,30 @@ search_code_matches = function(REGEX, FD, logged = NULL) {
     matchloc_list[[j]] = stringr::str_locate_all(text[matchline_list[[j]]], REGEX$regex)
   }
   
-  ## Format matched results properly
+  ## Format the regular expression match results properly
   res = MatchedCodebase(CB = all_code, CB_subset = files_with_matches, matchlines = matchline_list, matchlocs = matchloc_list, REGEX = REGEX)
   
-  ## Log if necessary. Then return. 
+  ## Create a search log? (This outputs all the matches to a file.)
   if (!is.null(logged)) { create_search_log(logtype = logged, MCB = res) }
   
   return(res)
 }
 
 
-#' Adds comments into the code, and write to file if necessary
+#' Inserts comments into code
 #' 
-#' @param MCB Regex match list : this is output of search-code-matches
-#' @param add_comment If non-NULL, this is added to the source code as a next-line comment
-#' @param comment_heads Length 2 vector: A short and long comment header to add to comments
-#' @param mark If TRUE: Includes as a comment the location of replaced text 
-#' @param mark_replace_len If non-NULL, should be nchar of replacement (and the markers will be made in this length)
-#' @param marker Marker character : defaults to "*"
-#' @param write Write comments to code? (FALSE is essentially for a test run, or to make modifications that are manually written later)
+#' This function can insert comments into code, and also write to (replace) the original code files as necessary. 
+#' For example, this can be called when code replacement functions are called, if comments are desired to be left in the code to denote that things had been changed. 
 #' 
-#' @return Modified version of the match list
+#' @param MCB [] :: Regex match list : this is output of search-code-matches
+#' @param add_comment [char] :: If non-NULL, this is the comment added to the next line in the source code
+#' @param comment_heads [vector, char, length = 2] :: The first entry is a short comment header to add to comments. The second entry is a long comment header. (eg #| vs #|----## as comment headers)
+#' @param mark [logical] :: If TRUE: Includes as a comment the location of replaced text 
+#' @param mark_replace_len [numeric] :: If non-NULL, should be nchar of replacement (and the markers will be made in this length)
+#' @param marker [char, "*"] :: Marker character
+#' @param write [logical] :: Write comments to code? (FALSE is essentially for a test run, or to make modifications that are manually written later -- the output MCB will be updated, but not be written)
+#' 
+#' @return [] :: Modified version of the match list
 #' 
 #' @export
 #' 
