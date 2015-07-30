13277eca208549a2c32d5eff7cb26d65e22eaa3b
Lawrence Wang <625lawrence@gmail.com>
2015-07-30 19:53:00 -0400

    update example

--- examples_lawrence/netcompLib_setup.R
+++ examples_lawrence/netcompLib_setup.R
@@ -5,6 +5,9 @@ library(codeProcessing)
 
 DIR = FilesDescription(dirlist = "../netcompLib/netcompLib/R/", mode = "R")
 
+search_code(Regex("NetworkModel[A-Za-z]*"), FD = DIR)
+
+
 plot_dependency(FD = DIR, out_file = "test.pdf")
 
 generate_todolist(DIR)
