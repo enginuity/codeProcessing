058248fd4911b354db20b2c9dbac27cbdec32ca2
Lawrence Wang <625lawrence@gmail.com>
2015-10-05 14:31:41 -0400

    update example code

--- examples_lawrence/all_network_setup.R
+++ examples_lawrence/all_network_setup.R
@@ -5,13 +5,13 @@ library(codeProcessing)
 
 DIR = FilesDescription(dirlist = c("../smoothingGraphs/", "../netcompLib/", "../netcompSBM", "../../network-comparison/netcomp-project/"), mode = "R")
 
-search_code("fast_compute_pval", FD = DIR, add_comment = "Function obseleted. These simulations should use newer code (computePval/sim_hyptest) instead...")
+search_code("computeLik", FD = DIR, add_comment = "Function output and input changed -- output is now a list")
 
 search_code("recycle_fitstructs", FD = DIR, add_comment = "In set_sim_param, recycle_fitstructs is renamed into fitstruct_method")
 
 search_code("compute_pval", FD = DIR, add_comment = "Function obseleted. These simulations should use newer code (computePval/sim_hyptest) instead...")
 
-search_code("computeDfAdj", FD = DIR, add_comment = "** MUST EDIT -- TODO -- hidden_nodes parameter renamed to hidden_edges")
+search_code("NetworkModelPair", FD = DIR, add_comment = "Parameters changed")
 
 
 plot_dependency(FD = FilesDescription(dirlist = c("../netcompLib/")), out_file = "test.pdf")
