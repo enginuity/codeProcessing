418a9679f18ec7febf095e884085a40a61fb4494
Lawrence Wang <625lawrence@gmail.com>
2016-01-11 20:05:28 -0500

    rewrite todo generator

--- codeProcessing/R/generate_todo_list.R
+++ codeProcessing/R/generate_todo_list.R
@@ -3,7 +3,6 @@
 
 ## TODO: [C-functionality Extension] add C processing also to this. 
 
-
 #' Generates TODO list
 #'    
 #' This function generates a TODO list by looking for all #/## TODO: in the code base. 
@@ -20,26 +19,52 @@ generate_todolist = function(FD, outFile = "GEN_todolist.txt") {
   
   ## Find files
   all_code = extract_Codebase(FD = FD)
-
+  
   todo_type = NULL
   todolist = NULL
-
+  
   for(j in seq_along(all_code$files)) {
-    addition = paste("--In file:", all_code$files[[j]])
     f = all_code$code[[j]]
-    ## Grab all TODO entries, that start a line
-    linematches = grep("^ *#+ TODO:", f)  
     
-    ## TODO: [Refactor] Generalize this sort of printing, or use the printing mechanisms used in search_code and others. 
+    ## Grab line numbers for complete header documentation
+    match_long = grep("^ *#{1,2}@(S|L)", f)
+    
+    ## Grab line number matches for TODOs. (only grabs leading TODOs) 
+    linematches = grep("^ *#+ TODO:", f)
+    
+    ## Grab line numbers and function names
+    temp = extract_all_docu(FD)
+    
+    ## Make a table for TODOs and functions
+    if (length(linematches) > 0) { 
+      todotable = data.frame(lines = linematches, text = f[linematches])
+    } else {
+      todotable = NULL
+    }
+    if (length(temp$fxtables$fx_name > 0) { 
+      fxtable = data.frame(lines = temp$fxtables$fx_start, text = paste("***FunctionStart***", temp$fxtables$fx_name)))
+    } else {
+      fxtable = NULL
+    }
+    
+    ## Count todo TYPE
     if (length(linematches) > 0) {
-        todolist = c(todolist, addition, 
-          paste(stringr::str_pad(string = linematches, width = 5, side = "right"),
-              gsub("^ *", "", f[linematches]), sep = " ::: "), 
-        "##########", "##########", "##########")
       todo_type = c(todo_type, gsub("^ *#+ TODO: ", "", f[linematches]))
     }
+    
+    ## TODO: [Refactor] Generalize this sort of printing, or use the printing mechanisms used in search_code and others. 
+    tab = rbind(todotable, fxtable)
+    
+    todolist = c(todolist, 
+                 paste("--In file:", all_code$files[[j]]), ## Identify File
+                 "Description---", f[match_long], ## Add description
+                 paste(stringr::str_pad(string = tab$lines, width = 5, side = "right"), 
+                       tab$text, sep = " ::: "), ## Add todo/function
+                 "##########", "##########", "##########", "") ## Add separator
+    )
   }
   
+  ## Add TODO analysis
   ## Output alternate note, if no TODO's. 
   if (length(todolist) == 0) {
     todolist = "No outstanding '# TODO:' 's"
@@ -48,15 +73,15 @@ generate_todolist = function(FD, outFile = "GEN_todolist.txt") {
     specific_typed = grep("(\\[.*\\])|(--)", first_word)
     table_type = table(first_word[specific_typed])
     todolist = c(todolist, 
-      paste("----- There are", length(first_word) ,"outstanding TODO lines"),
-      paste("----- There are", length(first_word) - length(specific_typed) ,"unsorted TODO lines"),
-      "-----")
+                 paste("----- There are", length(first_word) ,"outstanding TODO lines"),
+                 paste("----- There are", length(first_word) - length(specific_typed) ,"unsorted TODO lines"),
+                 "-----")
     max_len = max(nchar(names(table_type))) + 1
     for(j in 1:length(table_type)) {
       todolist = c(todolist, 
-        paste("-----", 
-              stringr::str_pad(names(table_type)[j], width = max_len, side = 'right'),
-              "**", stringr::str_pad(table_type[j], width = 3, side = 'right'), sep = " "))
+                   paste("-----", 
+                         stringr::str_pad(names(table_type)[j], width = max_len, side = 'right'),
+                         "**", stringr::str_pad(table_type[j], width = 3, side = 'right'), sep = " "))
     }
   }
   
