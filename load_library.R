9c207433a9532745e621013b2d60d49526fb017a
Lawrence Wang <625lawrence@gmail.com>
2017-10-28 01:45:43 -0700

    Aggregate up packge loading scripts
    Cleaned up and put package experimentation scripts all into a single file.

--- load_library.R
+++
@@ -1,8 +0,0 @@
-##@S Loads all needed libraries and functions
-##@S Sourcing this file (from this directory) will load all the functions as a library (this does require the library to be built)
-
-## Load own library functions
-library(codeProcessing)
-
-## Source other libraries
-require(stringr)
